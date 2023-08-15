bc38d7106d0e25be3ae304d6d9456885c67eebcf
Srimoyee Dutta <79530134+sri23mo12yee01@users.noreply.github.com>
2023-08-15 09:14:47 +0000

    Delete src directory

--- src/main/java/com/Project/CreditCard/exceptions/CustomerExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.Project.CreditCard.exceptions;
-
-public class CustomerExistsException extends Exception{
-    public CustomerExistsException(String message) {
-        super(message);
-    }
-}
